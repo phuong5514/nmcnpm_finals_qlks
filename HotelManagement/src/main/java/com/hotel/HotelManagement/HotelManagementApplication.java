fde3e78c32aa9e39d0f06abdabc04a1270845ce6
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2024-12-22 15:19:55 +0700

    add files from other members

--- HotelManagement/src/main/java/com/hotel/HotelManagement/HotelManagementApplication.java
+++ HotelManagement/src/main/java/com/hotel/HotelManagement/HotelManagementApplication.java
@@ -2,6 +2,7 @@ package com.hotel.HotelManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HotelManagementApplication {
@@ -9,5 +10,6 @@ public class HotelManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelManagementApplication.class, args);
 	}
+
 
 }
